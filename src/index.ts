f2e0be4bdd4401e0194c5befd346d3bbd6a1b075
Bodya Kutsyk <mkkbodya@gmail.com>
2024-10-14 19:33:08 +0300

    added psql, sequilize

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
-import { app } from "./app.js";
+'use strict';
 
-const hostname = "127.0.0.1";
-const port = 3000;
+import cors from 'cors';
+import express from 'express';
+import userRouter from './routes/user.route.js';
+import expenseRoute from './routes/expense.route.js';
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Node js accounting!');
 });
+
+app.use('/users', userRouter);
+app.use('/expenses', expenseRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}!`)
+})
